47dddd362b4ed1fd23a35d06f63d957c7aac53eb
Manoel Paulino <mpaulino.me@gmail.com>
2022-10-17 17:53:21 -0300

    up

---
+++ src/Endpoints/Categories/CategoryGetAll.cs
@@ -0,0 +1,19 @@
+﻿using ApiFull.Domain.Products;
+using ApiFull.Infra.Data;
+
+namespace ApiFull.Endpoints.Categories;
+
+public class CategoryGetAll
+{
+    public static string Template => "/categories";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(ApplicationDbContext context)
+    {
+        var categories = context.Categories.ToList();
+        var response = categories.Select(categories => new CategoryResponse { Id = c.Id, Name = c.Name, Active = categories.Active });
+
+        return Results.Ok(response);
+    }
+}
