3954ec52a38f305382b691a927465ae7e843e2b7
Manoel Paulino <mpaulino.me@gmail.com>
2022-10-28 21:22:23 -0300

    up

--- src/Program.cs
+++ src/Program.cs
@@ -18,9 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
 app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Handle);
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
